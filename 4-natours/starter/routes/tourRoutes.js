725f3ee28c2a20b42223461bd474aad37ba2a39b
Danny Lam <dannny.lam@gmail.com>
2023-01-06 13:42:45 +1100

    api features class

---
+++ 4-natours/starter/routes/tourRoutes.js
@@ -0,0 +1,20 @@
+const express = require('express');
+const {
+  getAllTours,
+  getTour,
+  createTour,
+  updateTour,
+  deleteTour,
+  aliasTopTours,
+} = require('../controllers/tourControllers');
+
+const router = express.Router();
+
+// router.param('id', checkID);
+
+router.route('/top-5-cheap').get(aliasTopTours, getAllTours);
+
+router.route('/').get(getAllTours).post(createTour);
+router.route('/:id').get(getTour).patch(updateTour).delete(deleteTour);
+
+module.exports = router;
